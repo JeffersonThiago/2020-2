e980cb2b8187cceb7f30ecec0cad441033c0b952
Jefferson Thiago <jeffersonthiago98@hotmail.com>
2021-02-04 04:55:30 +0000

    Incrementos após aula do dia 19/01/2021

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Pergunta, Opcao
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Bem Vindo a aplicação de enquetes")
+
+class IndexView(generic.ListView):
+    template_name = 'enquetes/index.html'
+    context_object_name = 'ultimas_enquetes'
+    def get_queryset(self):
+        return Pergunta.objects.order_by('data_publicacao')[:5]
+
+class DetalhesView(generic.DetailView):
+    model = Pergunta
+    context_object_name = 'enquete'
+    template_name = 'enquetes/detalhes.html'
+
+class ResultadoView(generic.DetailView):
+    model = Pergunta
+    context_object_name = 'enquete'
+    template_name = 'enquetes/resultados.html'
+
+def votacao(request, enquete_id):
+    enquete = get_object_or_404(Pergunta, pk=enquete_id)
+    try:
+        op_desejada = enquete.opcao_set.get(pk = request.POST['opcao'])
+    except (KeyError, Opcao.DoesNotExist):
+        contexto = {
+            'enquete': enquete,
+            'error_message': "Deve ser selecionada uma opção valida"
+        }
+        return render(request, 'enquetes:detalhes', contexto)
+    else:
+        op_desejada.votos += 1
+        op_desejada.save()
+        return HttpResponseRedirect(reverse('enquetes:resultado',
+            args=(enquete.id,)))
